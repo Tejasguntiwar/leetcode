b1b199494b8e683adfa41905fd1d558cd5328d5f
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-12 23:15:47 +0530

    Time: 6 ms (95.71%) | Memory: 46.9 MB (83.82%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -6,13 +6,18 @@ class Solution {
         //     set.add(i);
         // }
         // int n = set.size();
-        // HashSet<Integer> set2 = new HashSet<>();
+        // HashMap<Integer,Integer> map = new HashMap<>();
         // int l = 0, r = 0;
-        // while(r<nums.length) {
-        //     set2.add(nums[r]);
-        //     if(set2.size() == n) {
-        //         c++;
-        //         l++;
+        // while(r<nums.length && l < nums.length) {
+        //     // if(map.get(l) == map.get(r)) l++;
+        //     map.put(nums[r], r);
+        //     if(map.size() == n) {
+        //         c+=nums.length-r;
+        //         if(map.get(nums[l]) == l) {
+        //             map.remove(nums[l]);
+        //             l++;
+        //         }
+        //         // continue;
         //     }
         //     r++;
         // }
@@ -23,18 +28,37 @@ class Solution {
             set.add(i);
         }
         int n = set.size();
-        System.out.println(n);
-        set.clear();
-        for(int i = 0; i<nums.length; i++) {
-            for(int j = i; j<nums.length; j++) {
-                set.add(nums[j]);
-                if(set.size() == n) {
-                    c++;
-                    // break;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int l = 0, r = 0;
+        while(r<nums.length && l < nums.length) {
+            map.put(nums[r], r);
+            if(map.size() == n) {
+                while(map.get(nums[l]) > l) {
+                    l++;
                 }
+                c+=(l+1);
             }
-            set.clear();
+            r++;
         }
         return c;
+        // int c = 0;
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i : nums) {
+        //     set.add(i);
+        // }
+        // int n = set.size();
+        // System.out.println(n);
+        // set.clear();
+        // for(int i = 0; i<nums.length; i++) {
+        //     for(int j = i; j<nums.length; j++) {
+        //         set.add(nums[j]);
+        //         if(set.size() == n) {
+        //             c++;
+        //             // break;
+        //         }
+        //     }
+        //     set.clear();
+        // }
+        // return c;
     }
 }
