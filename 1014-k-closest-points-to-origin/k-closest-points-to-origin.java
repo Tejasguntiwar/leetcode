cd57c0f8718967b0d1df17094e47f836e7b116b2
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-18 20:02:48 +0530

    Time: 29 ms (71.14%) | Memory: 59.6 MB (9.54%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int  n= points.length;
+        // PriorityQueue<Double> q = new PriorityQueue<>(
+        //     (a,b) ->Double.compare(a, b)
+        // );
+        PriorityQueue<Integer> q = new PriorityQueue<>(
+            (idx1,idx2) -> {
+                int dist1 = points[idx1][0] * points[idx1][0] + points[idx1][1] * points[idx1][1];
+                int dist2 = points[idx2][0] * points[idx2][0] + points[idx2][1] * points[idx2][1];
+                return Integer.compare(dist1,dist2);
+            }
+        );
+        HashMap<Double, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            // int n1diff = points[i][0] * points[i][0];
+            // int n2diff = points[i][1] * points[i][1];
+            // double sqrt = Math.sqrt(n1diff + n2diff);
+            // q.add(sqrt);
+            q.add(i);
+            // map.put(sqrt, i);
+        }
+        int[][] closest = new int[k][2];
+        for(int i = 0; i < k; i++) {
+            // closest[i][0] = points[map.get(q.peek())][0];
+            // closest[i][1] = points[map.get(q.remove())][1];
+            closest[i] = points[q.poll()];
+        }
+        return closest;
+    }
+}
