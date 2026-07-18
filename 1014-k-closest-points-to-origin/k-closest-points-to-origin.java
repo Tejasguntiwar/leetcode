113f6b5f92fc5d0a4dfa9a68ca7b022360806fd0
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-18 20:08:36 +0530

    Time: 22 ms (88.96%) | Memory: 51.4 MB (97.20%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -4,10 +4,20 @@ class Solution {
         // PriorityQueue<Double> q = new PriorityQueue<>(
         //     (a,b) ->Double.compare(a, b)
         // );
-        PriorityQueue<Integer> q = new PriorityQueue<>(
-            (idx1,idx2) -> {
-                int dist1 = points[idx1][0] * points[idx1][0] + points[idx1][1] * points[idx1][1];
-                int dist2 = points[idx2][0] * points[idx2][0] + points[idx2][1] * points[idx2][1];
+        // PriorityQueue<Integer> q = new PriorityQueue<>(
+        //     (idx1,idx2) -> {
+        //         int dist1 = points[idx1][0] * points[idx1][0] + points[idx1][1] * points[idx1][1];
+        //         int dist2 = points[idx2][0] * points[idx2][0] + points[idx2][1] * points[idx2][1];
+        //         return Integer.compare(dist1,dist2);
+        //     }
+        // );
+        
+        //OR
+        
+        PriorityQueue<int[]> q = new PriorityQueue<>(
+            (p1,p2) -> {
+                int dist1 = p1[0] * p1[0] + p1[1] * p1[1];
+                int dist2 = p2[0] * p2[0] + p2[1] * p2[1];
                 return Integer.compare(dist1,dist2);
             }
         );
@@ -17,14 +27,15 @@ class Solution {
             // int n2diff = points[i][1] * points[i][1];
             // double sqrt = Math.sqrt(n1diff + n2diff);
             // q.add(sqrt);
-            q.add(i);
+            // q.add(i);
+            q.add(points[i]);
             // map.put(sqrt, i);
         }
         int[][] closest = new int[k][2];
         for(int i = 0; i < k; i++) {
             // closest[i][0] = points[map.get(q.peek())][0];
             // closest[i][1] = points[map.get(q.remove())][1];
-            closest[i] = points[q.poll()];
+            closest[i] = q.poll();
         }
         return closest;
     }
