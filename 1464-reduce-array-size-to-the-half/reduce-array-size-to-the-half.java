b1a76c99e11d60f7dc97c7426a3ee40a2dcec207
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-22 20:14:31 +0530

    Time: 43 ms (23.90%) | Memory: 85.1 MB (84.78%) - LeetSync

---
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        int n = arr.length/2;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i < arr.length; i++) {
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(
+            (a,b) -> Integer.compare(b.getValue(),a.getValue())
+        );
+        for(Map.Entry<Integer,Integer> en: map.entrySet()) {
+            pq.add(en);
+        }
+        // OR
+        // PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        int size= 0, c = 0;
+        while(!pq.isEmpty()) {
+            int a = pq.remove().getValue();
+            size += a;
+            c++;
+            if(size>= n) {
+                break;
+            }
+        }
+        return c;
+    }
+}
