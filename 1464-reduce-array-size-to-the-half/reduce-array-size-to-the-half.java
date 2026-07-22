8117cb10ee13f40069967e2f18d376b5077af098
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-22 20:46:13 +0530

    Time: 54 ms (6.12%) | Memory: 84.9 MB (87.77%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -5,14 +5,14 @@ class Solution {
         for(int i = 0;i < arr.length; i++) {
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(
-            (a,b) -> Integer.compare(b.getValue(),a.getValue())
-        );
+        // PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(
+        //     (a,b) -> Integer.compare(b.getValue(),a.getValue())
+        // );
+        // OR
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue(Comparator.reverseOrder()));
         for(Map.Entry<Integer,Integer> en: map.entrySet()) {
             pq.add(en);
         }
-        // OR
-        // PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue(Comparator.reverseOrder()));
         int size= 0, c = 0;
         while(!pq.isEmpty()) {
             int a = pq.remove().getValue();
