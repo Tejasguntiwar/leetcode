a52e239e3bb6295e415789dd7c7019926e7cc486
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-27 20:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.16%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,5 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // int[] newArr = new int[n+m];
+        // for(int i = 0; i <m; i++) {
+        //     newArr[i] = nums1[i];
+        // }
+        // for(int i = m; i <m+n; i++) {
+        //     newArr[i] = nums2[i-m];
+        // }
+        // Arrays.sort(newArr);
+        
+        // for(int i = 0; i <m+n; i++) {
+        //     nums1[i] = newArr[i];
+        // }
+
+        
         // if(nums2.length==0) return;
         // if(nums1.length==0) return;
         // System.out.println(nums1.length);
@@ -7,17 +21,17 @@ class Solution {
         //     nums1[i] = nums2[i-m];
         // }
         // Arrays.sort(nums1);
-        int[] newArr = new int[n+m];
-        for(int i = 0; i <m; i++) {
-            newArr[i] = nums1[i];
-        }
-        for(int i = m; i <m+n; i++) {
-            newArr[i] = nums2[i-m];
-        }
-        Arrays.sort(newArr);
         
-        for(int i = 0; i <m+n; i++) {
-            nums1[i] = newArr[i];
+        int i = m-1, j = n-1, k = m+n-1;
+        while(i>=0 && j>=0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(j >= 0) {
+            nums1[k--] = nums2[j--];
         }
     }
 }
