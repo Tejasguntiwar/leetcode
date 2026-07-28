a3b11e1129d537381e22dc20f3c030d1a6cbe0ab
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-28 14:40:32 +0530

    Time: 901 ms (7.49%) | Memory: 60 MB (15.29%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -27,24 +27,49 @@
 //     }
 // }
 
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         HashSet<List<Integer>> set = new HashSet<>();
+
+//         for(int i = 0; i<nums.length; i++) {
+//             HashSet<Integer> seen = new HashSet<>();
+//             for(int j = i+1; j<nums.length; j++) {
+//                 int complement = -(nums[i]+nums[j]);
+
+//                 if(seen.contains(complement)) {
+//                     List<Integer> lis = Arrays.asList(nums[i], nums[j], complement);
+
+//                     Collections.sort(lis);
+
+//                     set.add(lis);
+//                 }
+//                 seen.add(nums[j]);
+//             }                
+//         }
+//         return new ArrayList<>(set);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        if(nums == null || n < 3) return new ArrayList<>();
+
+        Arrays.sort(nums);
         HashSet<List<Integer>> set = new HashSet<>();
-
-        for(int i = 0; i<nums.length; i++) {
-            HashSet<Integer> seen = new HashSet<>();
-            for(int j = i+1; j<nums.length; j++) {
-                int complement = -(nums[i]+nums[j]);
-
-                if(seen.contains(complement)) {
-                    List<Integer> lis = Arrays.asList(nums[i], nums[j], complement);
-
-                    Collections.sort(lis);
-
-                    set.add(lis);
-                }
-                seen.add(nums[j]);
-            }                
+        
+        for(int i = 0; i<n-2; i++) {
+            int l = i+1, r = n-1;
+            while(l<r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0) {
+                    set.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    r--;
+                } else if(sum<0) {
+                    l++;
+                } else r--;
+            }
         }
         return new ArrayList<>(set);
     }
