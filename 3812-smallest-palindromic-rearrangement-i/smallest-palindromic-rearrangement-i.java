a06981c30a03a5cc5838b168b5995c4a84fa649c
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-07-28 11:34:07 +0530

    Time: 21 ms (95.18%) | Memory: 48.2 MB (57.43%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,4 +1,33 @@
+// class Solution {
+//     public String smallestPalindrome(String s) {
+//         int n = s.length();
+//         int[] arr = new int[26];
+//         for(int i = 0; i < n; i++) {
+//             arr[s.charAt(i) - 'a']++;
+//         }
+//         char[] small= new char[n];
+//         int k = 0;
+//         for(int i = 0; i < arr.length; i++) {
+//             if(arr[i] == 0 || arr[i] == 1) continue;
+//             else {
+//                 while(arr[i]>1) {
+//                     small[k] =ch
+//                     small[n-k-1] = (char)(i + 'a');
+//                     k++;
+//                     arr[i]-=2;
+//                 } 
+//             }
+//         }
+//         for(int i = 0; i < arr.length; i++) {
+//             if(arr[i] == 1) small[k] = (char)(i + 'a');
+//         }
+//         return new String(small);
+//     }
+// }
 class Solution {
+
+    //  same as above just few modifications
+
     public String smallestPalindrome(String s) {
         int n = s.length();
         int[] arr = new int[26];
@@ -8,20 +37,22 @@ class Solution {
         char[] small= new char[n];
         int k = 0;
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == 0 || arr[i] == 1) continue;
-            else {
-                while(arr[i]>1) {
-                    small[k] =(char) (i + 'a');
-                    small[n-k-1] = (char)(i + 'a');
-                    // System.out.println(small[k] + " " + small[n-k-1]);
-                    k++;
-                    arr[i]-=2;
-                } 
+            if(arr[i] == 0) continue;
+            
+            char ch = (char) ( i + 'a');
+
+            if(arr[i] % 2 != 0) {
+                small[n/2] = ch;
+            }
+
+            int pairs = arr[i]/2;
+            for(int j = 0; j < pairs; j++) {
+                small[k] = ch;
+                small[n-k-1] = ch;
+                k++;
             }
         }
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == 1) small[k] = (char)(i + 'a');
-        }
+        
         return new String(small);
     }
 }
