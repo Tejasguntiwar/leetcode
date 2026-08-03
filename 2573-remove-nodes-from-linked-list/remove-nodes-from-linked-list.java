01e7df6d88eb00697f3e5833943dd115604b974f
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-08-03 22:04:29 +0530

    Time: 54 ms (38.82%) | Memory: 162.4 MB (5.08%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private void getNodes(ListNode node, ArrayList<Integer> list) {
+        if(node == null) return;
+        list.add(node.val);
+        getNodes(node.next, list);
+    }
+//     private void setNodes(ListNode node, Stack<Integer> st) {
+//         if(st.isEmpty()) return;
+// // In Java, when you pass an object reference into a method, it is passed by value. Reassigning node inside setNodes only points that local variable to a new memory address. It does not change the .next pointer of the parent node that called it.
+//         node = new ListNode(st.pop());
+//         setNodes(node.next, st);
+//     }
+    public ListNode removeNodes(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        getNodes(head,list);
+        
+        int max = list.get(list.size()-1);
+        
+        st.push(list.get(list.size()-1));
+        
+        for(int i = list.size()-2; i>=0; i--) {
+            if(max <= list.get(i)) {
+                max = list.get(i);
+                st.push(list.get(i));
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while(!st.isEmpty()) {
+            curr.next = new ListNode(st.pop());
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
