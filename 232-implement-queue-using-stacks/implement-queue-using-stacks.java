f7fcf746e128477c1c39c6b1477fd7ec1a34876e
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-08-07 00:56:39 +0530

    Time: 2 ms (17.25%) | Memory: 43.1 MB (15.40%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,115 @@
+// class MyQueue {
+//     Stack<Integer> s1;
+//     Stack<Integer> s2;
+//     public MyQueue() {
+//         s1 = new Stack<>();
+//         s2 = new Stack<>();
+//     }
+    
+//     public void push(int x) {
+//         while(!s1.isEmpty()) {
+//             s2.push(s1.pop());
+//         }
+//         s1.push(x);
+//         while(!s2.isEmpty()) {
+//             s1.push(s2.pop());
+//         }
+//     }
+    
+//     public int pop() {
+//         if(s1.isEmpty()) return -1;
+//         return s1.pop();
+//     }
+    
+//     public int peek() {
+//         if(s1.isEmpty()) return -1;
+//         return s1.peek();
+//     }
+    
+//     public boolean empty() {
+//         return s1.isEmpty();
+//     }
+// }
+
+// /**
+//  * Your MyQueue object will be instantiated and called as such:
+//  * MyQueue obj = new MyQueue();
+//  * obj.push(x);
+//  * int param_2 = obj.pop();
+//  * int param_3 = obj.peek();
+//  * boolean param_4 = obj.empty();
+//  */
+// class MyQueue {
+//     Stack<Integer> s1;
+//     Stack<Integer> s2;
+//     public MyQueue() {
+//         s1 = new Stack<>();
+//         s2 = new Stack<>();
+//     }
+    
+//     public void push(int x) {
+//         s1.push(x);
+//     }
+    
+//     public int pop() {
+//        if(!s1.isEmpty()) {
+//             while(!s1.isEmpty()) s2.push(s1.pop());
+//             int val = s2.pop();
+//             while(!s2.isEmpty()) s1.push(s2.pop());
+//             return val;
+//        } else return -1;
+//     }
+    
+//     public int peek() {
+//         if(!s1.isEmpty()) {
+//             while(!s1.isEmpty()) s2.push(s1.pop());
+//             int val = s2.peek();
+//             while(!s2.isEmpty()) s1.push(s2.pop());
+//             return val;
+//        } else return -1;
+//     }
+    
+//     public boolean empty() {
+//         if(s1.isEmpty()) return true;
+//         else return false;
+//     }
+// }
+class MyQueue {
+     Stack<Integer> s1;
+     Stack<Integer> s2;
+     public MyQueue() {
+         s1 = new Stack<>();
+         s2 = new Stack<>();
+     }
+
+    public void push(int x) {
+        while(!s1.isEmpty()) s2.push(s1.pop());
+        s1.push(x);
+        while(!s2.isEmpty()) s1.push(s2.pop());
+    }
+ 
+    public int pop() {
+	    if(s1.isEmpty()) return -1;
+    	return s1.pop();
+    }
+ 
+    public int peek() {
+        if(s1.isEmpty()) return -1;
+    	return s1.peek();
+    }
+ 
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
+
+
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
