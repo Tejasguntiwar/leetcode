89a1a8fcfed6f563db66479c99fd48961e710db4
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-08-11 15:56:57 +0530

    Time: 1 ms (14.47%) | Memory: 44.7 MB (15.89%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,10 +1,11 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         if(target.length == 0) return new ArrayList<String>();
+        int len = target.length;
         List<String> list = new ArrayList<>();
         Stack<Integer> st = new Stack<>();
         int j = 0;
-        for(int i = 1; i<= n && j<target.length; i++) {
+        for(int i = 1; i<= target[len-1]; i++) {
             st.push(i);
             if(target[j] != st.peek()) {
                 st.pop();
