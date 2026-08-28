c60f3f79dc50ba365b71b945b879a77c93459c6e
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-08-28 18:32:18 +0530

    Time: 6 ms (9.93%) | Memory: 55.2 MB (83.67%) - LeetSync

---
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        Arrays.sort(score, Comparator.comparingInt((int[] row) -> row[k]).reversed());
+        return score;
+    }
+}
