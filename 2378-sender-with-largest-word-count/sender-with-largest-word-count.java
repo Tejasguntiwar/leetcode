328ee072ed56b4651a09593eab88a208ba1d509d
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-09-02 17:23:29 +0530

    Time: 60 ms (32.22%) | Memory: 58.4 MB (19.63%) - LeetSync

--- 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
@@ -1,17 +1,41 @@
+// class Solution {
+//     public String largestWordCount(String[] msg, String[] sen) {
+//         HashMap<String, Integer> map = new HashMap<>();
+//         int max = Integer.MIN_VALUE;
+//         String maxName = "";
+//         for(int i =0;i < msg.length; i++) {
+//             map.put(sen[i], map.getOrDefault(sen[i], 0) + msg[i].split(" ").length);
+//         }
+//         for(String str : map.keySet()) {
+//             if(max <= map.get(str)) {
+//                 max = map.get(str);
+//                 maxName = str;
+//             }
+//         }
+//         return maxName;
+//     }
+// }
 class Solution {
     public String largestWordCount(String[] msg, String[] sen) {
-        TreeMap<String, Integer> map = new TreeMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         int max = Integer.MIN_VALUE;
         String maxName = "";
         for(int i =0;i < msg.length; i++) {
             map.put(sen[i], map.getOrDefault(sen[i], 0) + msg[i].split(" ").length);
-        }
-        for(String str : map.keySet()) {
-            if(max <= map.get(str)) {
-                max = map.get(str);
-                maxName = str;
+            if(max < map.get(sen[i])) { 
+                max = map.get(sen[i]);
+                maxName = sen[i];
+            } 
+            else if(max == map.get(sen[i])) {
+                maxName = maxName.compareTo(sen[i]) < 0 ? sen[i]: maxName;
             }
         }
+        // for(String str : map.keySet()) {
+        //     if(max <= map.get(str)) {
+        //         max = map.get(str);
+        //         maxName = str;
+        //     }
+        // }
         return maxName;
     }
 }
