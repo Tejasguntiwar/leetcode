f78c816a3ead30c36845a01b2e109f57080de492
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-09-05 11:28:45 +0530

    Time: 4 ms (88.55%) | Memory: 125.3 MB (98.09%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -3,17 +3,21 @@ class Solution {
         int s = -1, n = nums.length;
         int[] pre = new int[n];
         int[] post = new int[n];
-        pre[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            pre[i] = Math.max(pre[i-1],nums[i]);
-        }
+        
         post[n-1] = nums[n-1];
         for(int i = n-2; i >= 0; i--) {
             post[i] = Math.min(post[i+1],nums[i]);
         }
+
+        int max = 0;
         for(int i = 0; i < n; i++) {
-            if((pre[i] - post[i]) <= k) return i;
+            if(nums[i] > max) max = nums[i];
+            // pre[i] = Math.max(pre[i-1],nums[i]);
+            if((max - post[i]) <= k) return i; 
         }
+        // for(int i = 0; i < n; i++) {
+        //     if((pre[i] - post[i]) <= k) return i;
+        // }
         return s;
     }
 }
