db761f9c9805d3c8be9d3320034027f7c5258ae1
Tejas Guntiwar <guntiwar.tejas@gmail.com>
2026-09-06 14:53:02 +0530

    Time: 21 ms (85.41%) | Memory: 178.6 MB (7.99%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -1,3 +1,37 @@
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode modifiedList(int[] nums, ListNode head) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int n : nums) set.add(n);
+//         ListNode temp = head;
+//         while(set.contains(temp.val)) temp = temp.next;
+//         ListNode newHead = temp;
+//         ListNode last = temp;
+//         temp = temp.next;
+//         while(temp != null && temp.next != null) {
+//             while(temp != null && set.contains(temp.val)) {
+//                 last.next = temp.next;
+//                 temp = temp.next;
+//             }
+//             last = last.next;
+//             if(temp == null) break;
+//             temp = temp.next;
+//         }
+//         if(temp != null && set.contains(temp.val)) {
+//             last.next = null;
+//         }
+//         return newHead;
+//     }
+// }
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -12,23 +46,17 @@ class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
         HashSet<Integer> set = new HashSet<>();
         for(int n : nums) set.add(n);
-        ListNode temp = head;
-        while(set.contains(temp.val)) temp = temp.next;
-        ListNode newHead = temp;
-        ListNode last = temp;
-        temp = temp.next;
-        while(temp != null && temp.next != null) {
-            while(temp != null && set.contains(temp.val)) {
-                last.next = temp.next;
-                temp = temp.next;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode curr = dummy;
+        while(curr.next != null) {
+            if(set.contains(curr.next.val)) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
             }
-            last = last.next;
-            if(temp == null) break;
-            temp = temp.next;
         }
-        if(temp != null && set.contains(temp.val)) {
-            last.next = null;
-        }
-        return newHead;
+        return dummy.next;
     }
 }
